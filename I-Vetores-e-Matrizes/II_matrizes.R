f24b386b3f9779a42e1ca4c9ab54295caa5fc1f0
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-03-23 20:32:22 -0300

    Tipos de matrizes

--- I-Vetores-e-Matrizes/II_matrizes.R
+++ I-Vetores-e-Matrizes/II_matrizes.R
@@ -44,3 +44,98 @@ matriz4
 # Matriz com caracteres
 matriz5 <- matrix(c("segunda", "terça", "quarta", "quinta"), nrow=2, ncol=2, byrow=FALSE)
 matriz5
+
+# TIPOS DE MATRIZES
+
+# Matriz Linha
+matriz_linha <- matrix(c(10,30,-15,8), nrow=1)
+matriz_linha
+
+# Não confundir com o vetor
+vetor <- c(10,30,-15,8)
+vetor
+
+# Matrix somente com números 1
+matriz1 = matrix(data=1, nrow=8, ncol=7)
+matriz1
+
+matriz1 <- matrix(rep(1,12), nrow=3)
+matriz1
+
+# Matriz nula
+matriz0 <- matrix(rep(0,25), nrow=5)
+matriz0
+
+# Matriz Identidade
+matriz_ident <- diag(1, nrow=5)
+matriz_ident
+
+# Matriz diagonal
+matriz_diag <- diag(c(2, -3, 4, 7, 9, -11))
+matriz_diag
+
+# Matriz Transposta
+matriz <- matrix(c(1, 5, 10, 30, 15, 8), nrow=3, ncol=2, byrow=TRUE)
+matriz
+
+matriz_transp <- t(matriz)
+matriz_transp
+
+# Matriz oposta
+matriz_oposta <- (-1)*(matriz)
+matriz_oposta
+
+# MANIPULAÇÃO DE MATRIZES
+matriz <- matrix(c(1, 5, 30, 15, 8, 12), nrow=2, ncol=3, byrow=TRUE)
+matriz
+
+# Buscando elementos
+matriz[1, 2]
+matriz[3, 1]
+matriz[4, 1] # out of bound
+matriz[ , ]
+matriz[2, ]
+matriz[ , 1]
+
+# Substituindo valores
+matriz[1, 2] = 11
+matriz
+
+matriz[3, 2] = -12
+matriz
+
+# Criando matrizes ou vetores com elementos de outra matriz
+matriz2 <- matriz[1, ]
+matriz2
+class(matriz2)
+
+matriz
+
+matriz3 <- matriz[ , 2]
+matriz3
+
+matriz4 <- matriz[1:2, 1:2]
+matriz4
+
+# Média das linhas ou colunas
+matriz5 <- matrix(c(1, 5, 15, 8), nrow=2, ncol=2, byrow=TRUE)
+matriz5
+
+media_coluna <- colMeans(matriz5)
+media_coluna
+
+media_linha <- rowMeans(matriz5)
+media_linha
+
+# Soma de linhas ou colunas
+soma_linhas <- rowSums(matriz5)
+soma_linhas
+
+soma_colunas <- colSums(matriz5)
+soma_colunas
+
+######################################
+####### OPERAÇÕES COM MATRIZES #######
+######################################
+
+# ADIÇÃO E SUBTRAÇÃO DE MATRIZES
