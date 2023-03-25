c71a01c294e1f588b22447a9010f94415bee7621
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-03-25 11:00:47 -0300

    Inicialização MSE e adição de arquivo para testes

---
+++ II-Sistemas-Lineares/I_Vetores_e_Matrizes/II-Sistemas-Lineares/II_estimativa_de_minimos_quadrados.R
@@ -0,0 +1,13 @@
+################################################
+####### ESTIMATIVA POR MÍNIMOS QUADRADOS #######
+################################################
+
+# Análise e tratmento inicial
+install.packages("dplyr")
+library(dplyr)
+
+# Setar o diretório
+setwd("II-Sistemas-Lineares")
+getwd()
+
+# Abrir arquivo
