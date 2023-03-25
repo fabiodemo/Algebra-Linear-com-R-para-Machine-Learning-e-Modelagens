6f46a335364d4c1550acd8da01056f5d0d09cdbc
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-03-25 14:19:22 -0300

    Finalização do modelo de regressào linear multipla

--- II-Sistemas-Lineares/II_estimativa_por_minimos_quadrados.R
+++ II-Sistemas-Lineares/II_estimativa_por_minimos_quadrados.R
@@ -106,3 +106,60 @@ mse(resultado$nota, resultado$previsao)
 
 # Raiz do erro quadrático médio (RMSE)
 rmse(resultado$nota, resultado$previsao)
+
+###################################################
+####### MODELO DE REGRESSÃO LINEAR MÚLTIPLA #######
+###################################################
+
+View(resultado)
+
+# Criação da matriz dos coeficientes
+# e acrescentar uma coluna com 1 unidade
+resultado["unidade"] <- (resultado$inicio_estudo / resultado$inicio_estudo)
+resultado
+df3 <- subset(resultado, select=c(unidade, inicio_estudo, tempo_estudo_dia))
+df3
+
+B <- data.matrix(df3)
+B
+
+# Criação do vetor (matriz de constantes)
+df4 <- subset(resultado, select=c(nota))
+df4
+
+C <- data.matrix(df4)
+C
+
+# Estimativa por mínimos quadrados
+x <- solve(t(B) %*% B) %*% (t(B) %*% C)
+x
+
+# Equação
+# nota =  6.487128816 + (0.041330462 * inicio_estudo) + (0.006442735 * tempo_estudo_dia)
+
+# Construção do modelo de regressão linear multipla no R
+modelo <- lm(nota ~ inicio_estudo + tempo_estudo_dia, resultado)
+summary(modelo)
+
+
+# Métricas de desempenho
+View(resultado)
+
+# Acrescentando uma coluna comos valores previstos
+resultado["previsao2"] <- x[1, 1] + (resultado$inicio_estudo * x[2, 1]) + (resultado$tempo_estudo_dia * x[3, 1])
+View(resultado)
+resultado
+
+
+# Erro médio absoluto (MAE)
+mae(resultado$nota, resultado$previsao2)
+
+# Erro quadrático médio (MSE) - destaca os erros bem discrepantes
+mse(resultado$nota, resultado$previsao2)
+
+# Raiz do erro quadrático médio (RMSE)
+rmse(resultado$nota, resultado$previsao2)
+
+print(paste0("MAE previsao1: ", mae(resultado$nota, resultado$previsao), "   |    MAE previsao2: ", mae(resultado$nota, resultado$previsao2)))
+print(paste0("MSE previsao1: ", mse(resultado$nota, resultado$previsao), "   |    MSE previsao2: ", mse(resultado$nota, resultado$previsao2)))
+print(paste0("RMSE previsao1: ", rmse(resultado$nota, resultado$previsao), "   |   RMSE previsao2: ", rmse(resultado$nota, resultado$previsao2)))
