02fc8c031e2bc094321f9d017b8030a964deaf49
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-03-27 23:13:30 -0300

    Inserção do código PCA e finalização do curso

--- III-Transformação-Linear-Autovetores-Autovalores/VIII_PCA.R
+++ III-Transformação-Linear-Autovetores-Autovalores/VIII_PCA.R
@@ -2,4 +2,44 @@
 ####### Análise dos Componentes Principais (PCA) #######
 ##################################### 
 
+library(dplyr)
 
+getwd()
+setwd("III-Transformação-Linear-Autovetores-Autovalores")
+
+# Abrir arquivo com a massa de dois componentes químicos
+massa <- read.csv("inspecao.csv", sep=";")
+str(massa)
+
+# Verificação do tipo de atributos
+dim(massa)
+
+# Verificando valores ausentes
+sapply(massa, function(x) sum(is.na(x)))
+sapply(massa, function(x) sum(is.nan(x)))
+
+# Excluir valor NA
+massa2 <- na.omit(massa)
+massa2
+sapply(massa2, function(x) sum(is.na(x)))
+
+dim(massa2)
+
+# Criação da Matriz de covariância
+matriz <- subset(massa2, select=c(A,B))
+matriz
+
+matriz_cov <- cov(matriz)
+matriz_cov
+
+autov <- eigen(matriz_cov)
+autov
+
+# O autovalor 1.052 está associado aos autovetores -0.73 e -0.6821
+# O autovalor 0.1042 está associado aos autovetores 0.6821 e -0.7312
+
+# Cálculo da Taxa de Variância Explicada (EVR)
+EVR <- autov$values / sum(autov$values)
+EVR
+
+# Os componentes principais, neste caso, srão os autovetores -0,731 e -0.682
